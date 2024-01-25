454e82a956cfa5afe0a926143adc4e92742c0e2d
Sudharshan Muralidharan <50818126+sudharshan3@users.noreply.github.com>
2024-01-25 14:01:42 +0530

    dynamic credentials implemented

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+user=$(awk -F "=" '/user/ {print $2}' jenkins_job.ini)
+password=$(awk -F "=" '/password/ {print $2}' jenkins_job.ini)
 C:/Users/SudharshanMuralidhar/AppData/Local/Programs/Python/Python39/python.exe -m venv .
 source ./Scripts/activate      
 pip install jenkins-job-builder
 jenkins-jobs --conf ./jenkins_jobs.ini update  ./go-lms-pipeline.yaml --delete-old
-curl -X POST -u sudharshan:11f3c221d1cb742f65075af435602b1979 \
+curl -X POST -u "${user}":"${password}" \
   --data-urlencode json='{"parameter": [{"name":"PORT", "value":"4000"}]}' \
-  http://localhost:8080/job/new-test-jjb-35/build
+  http://localhost:8080/job/new-test-jjb-100/build
 
 
